fb0086bc2790a9376a084d781d3c5c737ae0db94
Skux4life <mattellis1234@gmail.com>
2024-11-27 15:35:19 +1100

    web components

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import Store from "./services/Store.js";
 import API from "./services/API.js";
 import { loadData } from "./services/Order.js";
 import Router from "./services/Router.js";
+// Link my Web Components
+import MenuPage from "./components/MenuPage.js";
+import DetailsPage from "./components/DetailsPage.js";
+import OrderPage from "./components/OrderPage.js";
 
 window.app = {};
 app.store = Store;
--- services/Router.js
+++ services/Router.js
@@ -1,16 +1,50 @@
 const Router = {
   init: () => {
-    document.querySelector("a.navlink").forEach((a) => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
       a.addEventListener("click", (event) => {
         event.preventDefault();
         const url = event.target.getAttribute("href");
         Router.go(url);
       });
     });
+    // Event handler for URL changes
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
     // Check the initial URL
     Router.go(location.pathname);
   },
-  go: (route, addToHistory = true) => {},
+  go: (route, addToHistory = true) => {
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("menu-page");
+        break;
+      case "/order":
+        pageElement = document.createElement("order-page");
+        break;
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("details-page");
+          pageElement.dataset.productId = route.substring(
+            route.lastIndexOf("/") + 1,
+          );
+        }
+        break;
+    }
+    if (pageElement) {
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    } else {
+      // 404
+      document.querySelector("main").innerHTML = "Oops, 404";
+    }
+  },
 };
 
 export default Router;
